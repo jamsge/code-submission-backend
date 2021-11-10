9f40cfd350b211837dc85669025e0a4a907fa547
jamsge <james.ge1@outlook.com>
2021-11-09 20:42:13 -0800

    mock submit route

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('server is running :)')
+})
+
+app.post('/submit', (req, res) => {
+    console.log(req.body)
+    res.json({
+        "success": true,
+        "input": req.body.code,
+        "output": "sample output",
+        "time": 0.69
+    })
 })
 
 app.listen(port, () => {
