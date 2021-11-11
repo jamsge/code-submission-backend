fe34d32eb154d80a88f244747c77a04f4a0c7b8e
Luciano Lim <chunyumikey@gmail.com>
2021-11-10 16:57:54 -0800

    adds cors to submit

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('server is running :)')
-})
+app.get("/", (req, res) => {
+  res.send("server is running :)");
+});
 
-app.post('/submit', (req, res) => {
-    console.log(req.body)
-    res.json({
-        "success": true,
-        "input": req.body.code,
-        "output": "sample output",
-        "time": 0.69
-    })
-})
+app.options("/submit", cors());
+app.post("/submit", cors(), (req, res) => {
+  console.log(req.body);
+  res.json({
+    success: true,
+    input: req.body.code,
+    output: "sample output",
+    time: 0.69,
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
